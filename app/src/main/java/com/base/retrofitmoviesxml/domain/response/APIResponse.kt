eebc1449898b8862823344571594e55544123fc1
Shweta-Fulzele <shweta.fulzele@thinkitive.com>
2023-12-05 17:47:24 +0530

    minor changes for API client

--- app/src/main/java/com/base/retrofitmoviesxml/domain/response/APIResponse.kt
+++
@@ -1,7 +0,0 @@
-package com.base.retrofitmoviesxml.domain.response
-
-sealed class APIResponse<out T> {
-    data class Success<T>(val data: T) : APIResponse<T>()
-    data class Error(val code: Int, val message: String) : APIResponse<Nothing>()
-    object Loading : APIResponse<Nothing>()
-}
---
+++ app/src/main/java/com/base/retrofitmoviesxml/utils/ApiConstants.kt
@@ -0,0 +1,8 @@
+package com.base.retrofitmoviesxml.utils
+
+ class ApiConstants {
+
+     companion object {
+         const val MOVIE_POPULAR = "movie/popular"
+     }
+ }
